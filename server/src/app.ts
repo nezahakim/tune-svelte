0a7e2c6c46247b64011f3ff7a17378d71db82059
nezahakim <mugwanezahakimu@gmail.com>
2025-03-11 12:58:37 +0200

    v.0.0.1

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,7 +12,10 @@ const app = express();
 const httpServer = createServer(app);
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
