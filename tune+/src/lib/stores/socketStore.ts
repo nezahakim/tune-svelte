a59a000fe38f6280a4ebf540e06e7721a2eddeaf
nezahakim <mugwanezahakimu@gmail.com>
2025-03-11 13:17:54 +0200

    v.0.0.1

--- tune+/src/lib/stores/socketStore.ts
+++ tune+/src/lib/stores/socketStore.ts
@@ -1,50 +1,10 @@
-// import { writable, get } from 'svelte/store';
-// import { io, Socket } from 'socket.io-client';
-// import { session } from '$lib/stores/session';
-
-// const SOCKET_URL = 'http://localhost:3000';
-
-// function createSocketStore() {
-//     const { subscribe, set, update } = writable<Socket | null>(null);
-
-//     return {
-//         subscribe,
-//         connect: () => {
-//             const currentSession = get(session);
-            
-//             // if (!currentSession.authenticated || !currentSession.user?.id) {
-//                 if (!session.isAuthenticated()) {
-//                 console.log(currentSession.authenticated, currentSession.user?.id);
-//                 console.error('Cannot connect socket: User not authenticated');
-//                 return null;
-//             }
-
-//             const socket = io(SOCKET_URL, {
-//                 auth: {
-//                     userId: currentSession.user.id,
-//                 }
-//             });
-
-//             set(socket);
-//             return socket;
-//         },
-//         disconnect: () => {
-//             update(socket => {
-//                 if (socket) socket.disconnect();
-//                 return null;
-//             });
-//         }
-//     };
-// }
-
-// export const socketStore = createSocketStore();
-
 import { writable, get } from 'svelte/store';
 import { io, Socket } from 'socket.io-client';
 import { session } from '$lib/stores/session';
 import { browser } from '$app/environment';
+import { API_BASE_URL } from '$lib/API_BASE';
 
-const SOCKET_URL = 'http://localhost:3000';
+const SOCKET_URL = API_BASE_URL;
 
 function createSocketStore() {
     const { subscribe, set, update } = writable<Socket | null>(null);
